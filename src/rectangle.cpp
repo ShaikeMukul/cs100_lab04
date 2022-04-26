46864ecb94d7c018e4e80f490f12ee164ab4aa89
ShaikeMukul <smuku004@ucr.edu>
2022-04-25 17:51:35 -0700

    some failed tests

---
+++ src/rectangle.cpp
@@ -0,0 +1,34 @@
+#include "../header/rectangle.hpp"
+
+Rectangle::Rectangle(){
+	this->width = 1;
+	this->height = 1;
+}
+
+Rectangle::Rectangle(int w, int h){
+	this->width = w;
+	this->height = h;
+}
+
+void Rectangle::set_width(int w) {
+    this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+    this->height = h;
+}
+
+int Rectangle::area() {
+    return this->width * this->height;
+}
+int Rectangle::get_width() {
+	return this->width;
+}
+
+int Rectangle::get_height() {
+	return this->height;
+
+}
+int Rectangle::perimeter(){
+	return (this->width*2 + this->height*2);
+}
