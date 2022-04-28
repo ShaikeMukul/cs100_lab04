038cc2f943dc0cdf5d66aa3bf7f2dacec87da757
Isean Bhanot <Isean4B@gmail.com>
2022-04-27 18:37:43 -0700

    Delete CMakeFiles directory

--- CMakeFiles/output.dir/src/rectangle.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/output.dir/src/rectangle.cpp.o: \
-  /Users/iseanbhanot/Documents/CS100/ibhan003-smuku004/src/rectangle.cpp \
-  /Users/iseanbhanot/Documents/CS100/ibhan003-smuku004/src/../header/rectangle.hpp
